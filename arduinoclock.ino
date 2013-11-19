cbc1b7f8740d9217a1cbf6920b15f85a1d2c110e
Sören Wegener <wegener92@gmail.com>
2013-11-19 16:36:06 +0100

    Finished LED matrix.

--- arduinoclock.ino
+++ arduinoclock.ino
@@ -8,16 +8,25 @@
 
 #include <Time.h>
 #include <OneWire.h>
-#include <DallasTemperature.h>
+//#include <DallasTemperature.h>
 
 #define TIME_HEADER  "T"   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
-#define BRIGHTNESS 0.2 // min. 0.2
+#define BRIGHTNESS 3 // min. 0.2
+
+#define HOUR_HAND_BRIGHTNESS 80 * BRIGHTNESS
+#define MINUTE_HAND_BRIGHTNESS 24 * BRIGHTNESS
+#define SECOND_HAND_BRIGHTNESS 5 * BRIGHTNESS
 
 #define ONE_WIRE_BUS 21
 #define FIRST_HAND_PORT 2
+#define CLOCK_CENTER_PORT 0
 //                   to 13 (total 12)
+#define MATRIX_LAST_ROW_PORT 52
+//                   to 44 in Zweierschritten
+#define MATRIX_FIRST_COL_PORT 37
+//                   to 53 in Zweierschritten
 
 static boolean NUMBERS [10][5][3] = {{
 {1, 1, 1},
@@ -80,15 +89,17 @@ static boolean NUMBERS [10][5][3] = {{
 {0, 0, 1},
 {1, 1, 1}}};
 
+/*
 boolean CELSIUS[5][3] = {
 {1, 1, 1},
 {1, 0, 0},
 {1, 0, 0},
 {1, 0, 0},
 {1, 1, 1}};
+*/
 
 OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+//DallasTemperature sensors(&oneWire);
 
 void setup() {
     Serial.begin(9600);
@@ -96,9 +107,15 @@ void setup() {
     {
         pinMode(i, OUTPUT);
     }
-    setSyncProvider( requestSync);  //set function to call when sync required
+    for (int i=MATRIX_FIRST_COL_PORT; i<MATRIX_FIRST_COL_PORT+16; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
+    pinMode(CLOCK_CENTER_PORT, OUTPUT);
+    digitalWrite(CLOCK_CENTER_PORT, 0);
+    setSyncProvider(requestSync);  // set function to call when sync required
     Serial.println("Waiting for sync message");
-    sensors.begin();
+    //sensors.begin();
 }
 
 void loop() {
@@ -119,77 +136,57 @@ void clock() {
     int second_hand = (current_second + 2) / 5 % 12;
     int minute_hand = (current_minute + 2) / 5 % 12;
     int hour_hand = current_hour % 12;
-    //Serial.println(current_minute);
-    //Serial.println(hand);
     for(int i=0; i<12; i++) {
         int port = i+FIRST_HAND_PORT - 1;
         if (i == 0) {
             port = i+FIRST_HAND_PORT + 11;
         }
         if (hour_hand==i) {
-            analogWrite(port, 80 * BRIGHTNESS);
+            analogWrite(port, HOUR_HAND_BRIGHTNESS);
         } else if (minute_hand==i) {
-            analogWrite(port, 24 * BRIGHTNESS);
+            analogWrite(port, MINUTE_HAND_BRIGHTNESS);
         } else if (second_hand==i) {
-            analogWrite(port, 5 * BRIGHTNESS);
+            analogWrite(port, SECOND_HAND_BRIGHTNESS);
         } else {
             analogWrite(port, 0);
         }
     }
-    Serial.print("Uhrzeit: ");
-    Serial.print(hour());
-    Serial.print(":");
-    Serial.print(minute());
-    Serial.print(":");
-    Serial.print(second());
-    Serial.println();
-    Serial.print("Sekundenzeiger: ");
-    Serial.print(second_hand);
-    Serial.println();
-    Serial.print("Minutenzeiger: ");
-    Serial.print(minute_hand);
-    Serial.println();
-    Serial.print("Stundenzeiger: ");
-    Serial.print(hour_hand);
-    Serial.println();
-    Serial.println();
+    analogWrite(CLOCK_CENTER_PORT, (current_second % 2) * SECOND_HAND_BRIGHTNESS);
 }
 
 void thermometer() {
     //multiplex(measure_temperature());
-    measure_temperature();
+    multiplex(millis() / 1000.0);
 }
 
+/*
 float measure_temperature() {
     sensors.requestTemperatures();
     return sensors.getTempCByIndex(0);
 }
+*/
 
 void multiplex(float value) {
     int a = int(value / 10) % 10;
     int b = int(value) % 10;
     int c = int(value * 10) % 10;
-    Serial.print(a);
-    Serial.print(b);
-    Serial.print(".");
-    Serial.print(c);
-    Serial.print("C");
-    Serial.println();
-    Serial.println();
+    int port = -1;
     for(int col=0; col<9; col++) {
         for(int row=0; row<5; row++) {
             int current_number;
+            port = MATRIX_LAST_ROW_PORT - row * 2;
             if (col < 3) {
-                digitalWrite(row, NUMBERS[a][row][col % 3]);
+                digitalWrite(port, NUMBERS[a][row][col % 3]);
             } else if (col < 6) {
-                digitalWrite(row, NUMBERS[b][row][col % 3]);
+                digitalWrite(port, NUMBERS[b][row][col % 3]);
             } else {
-                digitalWrite(row, NUMBERS[c][row][col % 3]);
+                digitalWrite(port, NUMBERS[c][row][col % 3]);
             }
         }
-        digitalWrite(col, HIGH);
-        delay(10);
-        digitalWrite(col, LOW);
+        port = MATRIX_FIRST_COL_PORT + col * 2;
+        digitalWrite(port, HIGH);
+        delay(1);
+        digitalWrite(port, LOW);
     }
 }
 
@@ -199,7 +196,7 @@ void processSyncMessage() {
 
     if(Serial.find(TIME_HEADER)) {
         pctime = Serial.parseInt();
-        if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+        if(pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
             setTime(pctime); // Sync Arduino clock to the time received on the serial port
         }
     }
